245fea705e4ec980b43e9f62eca93614c1baa410
oscar <oscar.lopezrojo@edu.uah.es>
2025-02-20 15:22:51 +0100

    Dibujo separado por funciones

---
+++ lab03-animation/lab03-animation.py
@@ -0,0 +1,15 @@
+import arcade
+
+arcade.open_window(800, 600, "Fortnite chamba")
+
+arcade.set_background_color(arcade.color.AIR_SUPERIORITY_BLUE)
+
+arcade.start_render()
+
+
+
+
+
+arcade.finish_render()
+arcade.run()
+
